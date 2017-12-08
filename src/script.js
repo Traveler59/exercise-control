71082f3bf800f1d0c34019ba778aa831bfe08c66
Traveler59 <dmitry_alekseevich@mail.ru>
2017-12-08 18:59:15 +0300

    Инициализация

--- src/script.js
+++ src/script.js
@@ -1,7 +1,19 @@
 const fs = require('fs');
+const readline = require('readline');
 
 const now = new Date();
 const fileName = 'src/data.json';
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const addToday = () =>
+    fs.writeFile(fileName, JSON.stringify({ previousDay: now }), err => {
+        if (err) {
+            return console.log(err);
+        }
+    });
 
 const getDay = (date) => date.getDate() === now.getDate()
     ? 'Сегодня'
@@ -11,21 +23,10 @@ const getDay = (date) => date.getDate() === now.getDate()
             ? 'Позавчера'
             : date;
 
-const askAdd = () => {
-    const readline = require('readline');
-
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-
-    rl.question('Добавить день?: ', answer => {
+const askAdd = (question = 'Добавить день?') =>
+    rl.question(question + ': ', answer => {
         if (answer.toString().trim() === 'y') {
-            fs.writeFile(fileName, JSON.stringify({ previousDay: now }), err => {
-                if (err) {
-                    return console.log(err);
-                }
-            });
+            addToday();
             console.log('День добавлен');
         } else {
             console.log('День не добавлен');
@@ -33,15 +34,14 @@ const askAdd = () => {
 
         setTimeout(() => rl.close(), 500)
     });
-}
-
 
 fs.readFile(fileName, 'utf8', (err, data) => {
     if (err) {
-        return console.log(err);
+        askAdd('Файла с данными нет. Создать файл и записать в него этот день?');
+        return;
     }
+
     const day = new Date(JSON.parse(data).previousDay);
     console.log('Предыдущий день:', getDay(day) === 'Сегодня' || getDay(day) === 'Вчера' ? '\x1b[32m' : '', getDay(day), '\x1b[0m');
-
     askAdd();
 });

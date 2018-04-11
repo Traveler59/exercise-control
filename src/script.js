adc587aa0e56227f7ec8b06bb8960d71d65503d6
Traveler59 <dmitry_alekseevich@mail.ru>
2018-04-11 15:36:46 +0300

    Подтверждение дня на Enter

--- src/script.js
+++ src/script.js
@@ -9,33 +9,24 @@ const readline = require('readline').createInterface({
 const now = new Date();
 const fileName = 'src/data.json';
 
-const startTimers = () => {
-    let time = 0;
+let dayAdded = false;
+let time = 0;
 
-    setInterval(() => {
-        time++;
-        const minutes = Math.floor(time / 60);
-        const seconds = time - 60 * minutes;
-        const format = number => number.toLocaleString('ru-RU', { minimumIntegerDigits: 2, useGrouping: false });
-        const rest = 300;
+const startTimers = () => setInterval(() => {
+    time++;
+    const minutes = Math.floor(time / 60);
+    const seconds = time - 60 * minutes;
+    const format = number => number.toLocaleString('ru-RU', { minimumIntegerDigits: 2, useGrouping: false });
+    const rest = 300;
 
-        const output = readline.output;
+    const output = readline.output;
 
-        output.clearLine();
-        output.cursorTo(0);
-        time > rest && output.write('\u{1b}[0;33m');
-        output.write(`${format(minutes)}:${format(seconds)}`)
-        time > rest && output.write('\u{1b}[0m');
-    }, 1000);
-
-    readline.input.on('keypress', (letter, key) => {
-        if (key && (key.name == 'enter' | key.name == 'return')) {
-            time = 0;
-        } else if (key && key.name == 'escape') {
-            process.exit();
-        }
-    });
-}
+    output.clearLine();
+    output.cursorTo(0);
+    time > rest && output.write('\u{1b}[0;33m');
+    output.write(`${format(minutes)}:${format(seconds)}`)
+    time > rest && output.write('\u{1b}[0m');
+}, 1000);
 
 const addToday = () => {
     fs.writeFile(fileName, JSON.stringify({ previousDay: now }), err => {
@@ -43,30 +34,41 @@ const addToday = () => {
             return console.log(err);
         }
     });
-
+    dayAdded = true;
     startTimers();
 }
 
-const askAdd = (question = 'Добавить день?') =>
-    readline.question(question + ': ', answer => {
-        if (answer === 'y') {
+const askAddDay = (question = 'Добавить день?') => console.log(question + ':');
+
+readline.input.on('keypress', (letter, key) => {
+    const isEnter = key && (key.name == 'enter' | key.name == 'return');
+    const isEscape = key && key.name == 'escape';
+
+    if (dayAdded) {
+        if (isEnter) {
+            time = 0;
+        } else if (isEscape) {
+            process.exit();
+        }
+    } else {
+        if (isEnter) {
             addToday();
             console.log('День добавлен');
-        } else {
+        } else if (isEscape) {
             console.log('День не добавлен');
-            setTimeout(() => readline.close(), 500)
         }
-    });
+    }
+});
 
 fs.readFile(fileName, 'utf8', (err, data) => {
     if (err) {
-        askAdd('Файла с данными нет. Создать файл и записать в него этот день?');
+        askAddDay('Файла с данными нет. Создать файл и записать в него этот день?');
         return;
     }
 
     const day = new Date(JSON.parse(data).previousDay);
 
-    const getDay = (date) =>
+    const getDay = date =>
         date.getDate() === now.getDate()
             ? 'Сегодня'
             : date.getDate() + 1 === now.getDate()
@@ -77,5 +79,5 @@ fs.readFile(fileName, 'utf8', (err, data) => {
 
     const formattedDay = getDay(day);
     console.log('Предыдущий день:', formattedDay === 'Сегодня' || formattedDay === 'Вчера' ? '\x1b[32m' : '', formattedDay, '\x1b[0m');
-    askAdd();
+    askAddDay();
 });
